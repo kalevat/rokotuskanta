6dab75d0c2b4a1cd7f84357ff57b42f0ba42f004
Tomi Kaleva <tomi.kaleva@gmail.com>
2021-03-26 14:11:40 +0000

    changes

--- messages.py
+++ messages.py
@@ -2,7 +2,7 @@ from db import db
 import users
 
 def get_list():
-    sql = "SELECT v.id, v.vacc, v.date, p.placename FROM vaccination v INNER JOIN place p ON p.id=v.place_id;"
+    sql = "SELECT u.username, v.vacc, v.date, p.placename FROM vaccination v INNER JOIN place p ON p.id=v.place_id INNER JOIN users u ON u.id=v.user_id;"
     result = db.session.execute(sql)
     return result.fetchall()
 
@@ -10,7 +10,7 @@ def send(name,place,vacc,date):
     user_id = users.user_id()
     if user_id == 0:
         return False
-    sql = "INSERT INTO vaccination (vacc, place_id, date) VALUES (:vacc, (SELECT id FROM place WHERE placename=:place), :date)"
-    db.session.execute(sql, {"vacc":vacc, "place":place, "date":date})
+    sql = "INSERT INTO vaccination (vacc, place_id, date, user_id) VALUES (:vacc, (SELECT id FROM place WHERE placename=:place), :date, (SELECT id FROM users WHERE username=:name))"
+    db.session.execute(sql, {"vacc":vacc, "place":place, "date":date, "name":name})
     db.session.commit()
     return True
--- schema.sql
+++ schema.sql
@@ -3,16 +3,17 @@ CREATE TABLE place (
 	placename TEXT
 );
 INSERT INTO place (placename) VALUES ('Helsinki'),('Turku'),('Vantaa');
+
 CREATE TABLE vaccination (
 	id SERIAL PRIMARY KEY,
 	vacc INT,
 	date TIMESTAMP,
-	place_id INTEGER REFERENCES place
+	place_id INTEGER REFERENCES place,
+	user_id INTEGER REFERENCES users
 );
 CREATE TABLE users (
 	id SERIAL PRIMARY KEY,
 	username TEXT UNIQUE,
 	password TEXT,
-	rights INT,
-	vaccination_id INTEGER REFERENCES vaccination
+	rights INT
 );
