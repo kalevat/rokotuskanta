7c0c069105cc05a38d2ee442e38fb23cd7667f38
Tomi Kaleva <tomi.kaleva@gmail.com>
2021-04-18 17:14:07 +0000

    login fixed

--- users.py
+++ users.py
@@ -1,6 +1,6 @@
 from db import db
 from flask import session
-#from werkzeug.security import check_password_hash, generate_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 
 
 def login(username, password):
@@ -10,22 +10,19 @@ def login(username, password):
     if user == None:
         return False
     else:
-        # if check_password_hash(user[0], password):
-        #     session["user_id"] = user[1]
-        #     return True
-        # else:
-        #     return False
-        session["user_id"] = user[1]
-        return True
-
+        if check_password_hash(user[0], password):
+            session["user_id"] = user[1]
+            return True
+        else:
+            return False
+ 
 
 def logout():
     del session["user_id"]
 
 
 def register(username, password):
-    #hash_value = generate_password_hash(password)
-    hash_value = password
+    hash_value = generate_password_hash(password)
     if username == "" or password == "":
         return False
     try:
