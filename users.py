f1ca3878d086e393f6fe6313cc5214b0d9c1cf8f
Tomi Kaleva <tomi.kaleva@gmail.com>
2021-04-18 16:48:20 +0000

    password changes

--- users.py
+++ users.py
@@ -1,6 +1,6 @@
 from db import db
 from flask import session
-from werkzeug.security import check_password_hash, generate_password_hash
+#from werkzeug.security import check_password_hash, generate_password_hash
 
 
 def login(username, password):
@@ -10,11 +10,13 @@ def login(username, password):
     if user == None:
         return False
     else:
-        if check_password_hash(user[0], password):
-            session["user_id"] = user[1]
-            return True
-        else:
-            return False
+        # if check_password_hash(user[0], password):
+        #     session["user_id"] = user[1]
+        #     return True
+        # else:
+        #     return False
+        session["user_id"] = user[1]
+        return True
 
 
 def logout():
@@ -22,7 +24,8 @@ def logout():
 
 
 def register(username, password):
-    hash_value = generate_password_hash(password)
+    #hash_value = generate_password_hash(password)
+    hash_value = password
     if username == "" or password == "":
         return False
     try:
